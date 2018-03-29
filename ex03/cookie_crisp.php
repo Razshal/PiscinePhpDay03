8acab8c0d47e5dc3aa701b0b8ac9eef6e58e783f
Razshal <fontenille.mathieu@gmail.com>
2018-03-29 15:00:21 +0200

    ex03 done

--- ex03/cookie_crisp.php
+++ ex03/cookie_crisp.php
@@ -4,13 +4,12 @@ if ($_GET["action"] === "set"
 {
     setcookie($_GET["name"], $_GET["value"], time() + (31 * 24 * 60 * 60));
 }
-else if ($_GET["action"] === "get" &&
-    $_GET["name"] != NULL && $_COOKIE[$_GET["name"]] != NULL)
+else if ($_GET["action"] === "get" && $_COOKIE[$_GET["name"]])
 {
     echo("{$_COOKIE[$_GET["name"]]}\n");
 }
 else if ($_GET["action"] === "del")
 {
-    setcookie($_GET["name"], NULL,time() - 1);
+    setcookie($_GET["name"], "", time() - 3600);
 }
 ?>
