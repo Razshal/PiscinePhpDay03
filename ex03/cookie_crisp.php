3bc1a947ff6e7d638821ae7c64f234c04f939546
Razshal <fontenille.mathieu@gmail.com>
2018-03-30 19:10:16 +0200

    light refactor2

--- ex03/cookie_crisp.php
+++ ex03/cookie_crisp.php
@@ -1,15 +1,8 @@
 <?php
-if ($_GET["action"] === "set"
-    && $_GET["name"] && $_GET["value"])
-{
+if ($_GET["action"] === "set" && $_GET["name"] && $_GET["value"])
     setcookie($_GET["name"], $_GET["value"], time() + (31 * 24 * 60 * 60));
-}
 else if ($_GET["action"] === "get" && $_COOKIE[$_GET["name"]])
-{
     echo("{$_COOKIE[$_GET["name"]]}\n");
-}
 else if ($_GET["action"] === "del")
-{
     setcookie($_GET["name"], "", time() - 3600);
-}
 ?>
